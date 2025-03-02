a301b0be57cdf522d49f54ec5895394bf06b8794
gowsika-muthusamy <gowsikag16@gmail.com>
2025-03-02 19:28:03 +0530

    add wordscalculator

---
+++ wordscalculator/script.js
@@ -0,0 +1,6 @@
+function convert(){
+    const str= document.getElementById("input").value
+     const count=str.length
+      const result1 =document.getElementById("result")
+    result1.innerHTML=count }
+   
